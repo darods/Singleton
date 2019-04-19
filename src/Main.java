a3f7b0bf168e18d9a7eaadbfc34ae7c6650fcdae
NASA <daniel_arod@protonmail.com>
2019-04-19 18:07:18 -0500

    añadido la solucion del singleton al hambiente multihilos

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        SingletonFirst singleton = SingletonFirst.getInstance();
-        singleton.mensaje("hola mundo! implementacion del singleton simple");
-        SingletonFirst singleton2 = SingletonFirst.getInstance();
-        System.out.println(singleton2.getMensaje());
 
+        Thread hilo1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SingletonFirst singleton = SingletonFirst.getInstance();
+                singleton.mensaje("hola mundo! implementacion del singleton simple");
+                System.out.println(singleton.getMensaje());
+            }
+        });
+
+        Thread hilo2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SingletonFirst singleton2 = SingletonFirst.getInstance();
+                singleton2.mensaje("otro hilo");
+                System.out.println(singleton2.getMensaje());
+            }
+        });
+
+        hilo1.start();
+        hilo2.start();
     }
 }
